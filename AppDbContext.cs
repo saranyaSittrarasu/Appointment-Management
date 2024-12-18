a1f6bda673e47fcc5e4a3c237f52126e8c17f709
Saranya Karthikeyan <saranya.karthik@outlook.com>
2024-12-18 18:45:28 +0100

    Initial commit

---
+++ AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using AppoinmentManagement.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppoinmentManagement
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Appointment> Appointments { get; set; }
+    }
+}
